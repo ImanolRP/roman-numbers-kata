f37343bee9b6a0bda16edd9c121bf084cc489a21
ImanolRP <imanol_ramirez@hotmail.com>
2023-03-27 20:02:53 +0200

    Refactor: tests

--- typescript/tests/RomanNumber.spec.ts
+++ typescript/tests/RomanNumber.spec.ts
@@ -1,21 +1,14 @@
 import { RomanNumber } from '../src/RomanNumber'
 
 describe('roman-number-kata', () => {
-  it('should return I when 1 given', () => {
+  it.each`
+    given | expected
+    ${1}  | ${'I'}
+    ${2}  | ${'II'}
+    ${3}  | ${'III'}
+  `('should return "$expected" when "$given" given', ({ given, expected }) => {
     const romanNumber = new RomanNumber()
 
-    expect(romanNumber.convert(1)).toBe('I')
-  })
-
-  it('should return II when 2 given', () => {
-    const romanNumber = new RomanNumber()
-
-    expect(romanNumber.convert(2)).toBe('II')
-  })
-
-  it('should return III when 3 given', () => {
-    const romanNumber = new RomanNumber()
-
-    expect(romanNumber.convert(3)).toBe('III')
+    expect(romanNumber.convert(given)).toBe(expected)
   })
 })
