63aa694cc94c325aba2167d46a133cd7e9942aaa
katarn <katarn@katarn.es>
2023-03-27 20:09:17 +0200

    Green: should return IV when 4 given

--- typescript/src/RomanNumber.ts
+++ typescript/src/RomanNumber.ts
@@ -1,9 +1,15 @@
 export class RomanNumber {
   convert (input: number): string {
+    if (input === 4) {
+      return 'IV'
+    }
+
     let response = ''
+
     for (let iterator = 0; iterator < input; iterator++) {
       response += 'I'
     }
+
     return response
   }
 }
